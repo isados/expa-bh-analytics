e56519d589af7f3d55bc0fef75e941615ebfdd94
Isa <isa.beec.14@acharya.ac.in>
2021-03-21 22:42:18 +0300

    now NaN values are replaced with blanks

--- run.py
+++ run.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
 
     #  Flatten dictionary and compress keys
     results = pd.json_normalize(results, sep='_')
-    results.replace([np.NaN], '-', inplace=True)
+    results.replace([np.NaN], '', inplace=True)
 
     results = json.loads(response.content)
 
