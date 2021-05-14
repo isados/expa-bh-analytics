c790aa50c3697003f13f569c5235441a6d28b581
Isa AlDoseri <isadosry@gmail.com>
2021-05-14 19:09:53 +0300

    get credentials for service account and AIESEC access token from `creds` folder

--- run_desktop.sh
+++ run_desktop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export ACCESS_TOKEN=7789a122c78d710a724f3cd2a7d8daf809b467d46cf63eafa76b2ae87a32f035
+export ACCESS_TOKEN=$(cat creds/access_token)
 
 # Service Account Credentials are stored in `base64encoded_creds` file
-export GOOGLE_CREDS=$(cat base64encoded_creds)
+export GOOGLE_CREDS=$(cat creds/base64encoded_creds)
 
 export SPREADSHEET_ID=1MQOyYpuJ4zF5xyqjVtAnrTrG3In9lxWl9L3yXiTeYiw
 export SHEET_NAME=HEROKU
